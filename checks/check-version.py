820e422101e282e03ca170134d1a7652415595d1
morph42 <octorandfan@gmail.com>
2024-01-10 21:26:55 +0000

    switched some more scripts to pure python

--- checks/check-version.py
+++ checks/check-version.py
@@ -2,10 +2,16 @@
 import re
 from helper import *
 
+def versiontuple(v):
+    return tuple(map(int, (v.split("."))))
+
 printHEADER("Check Versions")
 
 goal = runCommand(['goal','version','-v'])
-x = re.search(r"Build:\s+(\S+)",goal)
-printINFO("goal version - " + x.group(1))
+x = re.search(r"Build:\s+(\d+\.\d+\.\d+)",goal)
+version = x.group(1)
 
-if x.group(1) 
+printINFO("goal version - " + version)
+
+if (versiontuple(version) < versiontuple("3.21.0")):
+  printERROR("This version " + version + " is less than 3.21.0 - some scripts may not run")
