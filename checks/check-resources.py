6e1b018286513d0e5c706edebdf6465740621798
morph42 <octorandfan@gmail.com>
2024-01-11 19:26:32 +0000

    adding server stats (cpu/ram/uptime)

--- checks/check-resources.py
+++ checks/check-resources.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+import os
 import re
+import multiprocessing
 from helper import *
 
 highcpu = 80;
@@ -8,21 +10,38 @@ highhd = 95;
 
 printHEADER("Check Resources")
 
+# Gather uptime
+
+with open('/proc/uptime', 'r') as f:
+  uptime_seconds = float(f.readline().split()[0])
+  uptime_days = round(uptime_seconds / 60 / 60 / 24,2)
+
+printINFO("uptime is " + str(uptime_days) + " days")
+
+
+
 # CPU stats gathering 
+
+printINFO("CPU Count is " + str(multiprocessing.cpu_count()))
 
 cpucmd = re.search(r"(\d+)",runCommand(['checks/check-cpu.sh']))
 cpu = int(cpucmd.group(1))
 if (cpu < highcpu):
-  printOK("CPU - " + str(cpu) + "%")
+  printOK("CPU usage is currently - " + str(cpu) + "%")
 else:
   printERROR("CPU is high - " + str(cpu) + "%")
 
 # RAM stats gathering
 
+mem_bytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')  # e.g. 4015976448
+mem_gib = mem_bytes/(1024.**3)
+
+printINFO("RAM present is " + str(mem_gib) + "Gb")
+
 ramcmd = re.search(r"(\d+)",runCommand(['checks/check-mem.sh']))
 ram = int(ramcmd.group(1))
 if (ram < highram):
-  printOK("RAM - " + str(ram) + "%")
+  printOK("RAM usage is currently " + str(ram) + "%")
 else:
   printERROR("RAM is high - " + str(ram) + "%")
 
