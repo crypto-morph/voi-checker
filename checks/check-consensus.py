f964eb6f998b157ed81d7f99edc043c5ea860e03
morph42 <octorandfan@gmail.com>
2024-01-25 20:54:12 +0000

    check partkey output

--- checks/check-consensus.py
+++ checks/check-consensus.py
@@ -5,10 +5,14 @@ from helper import *
 
 printHEADER("Checking Consensus")
 addr = os.environ["addr"]
+avgPctOnTime = 0
 
 goal = runCommand(["curl", "-s", "https://analytics.testnet.voi.nodly.io/v0/consensus/accounts/all"])
 goalcmd = re.search(r"\s+\[\"" + addr + "\"\,\s+\"(\d+)\"\,\s+\"(\d+)\"\,\s+\"(\d+)\"\,\s+\"(\d+)\"\,\s+(\d+)\,\s+\"(\d+)\"\,\s+\"([^,]+)\"\,\s+([^,]+),\s+([^,]+),\s+(.+)\]\,",goal)
-avgPctOnTime = float(goalcmd.group(10))
+if goalcmd is not None: 
+  avgPctOnTime = float(goalcmd.group(10))
+else:
+  printERROR('Checker failed to get consensus output from analytics.testnet.voi.nodly.io')
 if (avgPctOnTime > 80):
   printOK("good average percentage on time - " + str(avgPctOnTime) + "%")
 else: 
